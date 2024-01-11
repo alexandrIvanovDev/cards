468e2a4fc3399762344b3a06ea00116cbf2eeda9
AlexandrIvanov <alexandr.1996@list.ru>
2024-01-11 18:23:54 +0300

    Add react-router decorator for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,8 +2,10 @@ import type { Preview } from '@storybook/react'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '../src/app/styles/index.scss'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const preview: Preview = {
+  decorators: [withRouter],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
