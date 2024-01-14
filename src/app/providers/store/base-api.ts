77567d1c9177268d5619f8720c211e00655d5a45
AlexandrIvanov <alexandr.1996@list.ru>
2024-01-14 12:55:30 +0300

    Add directory for store

--- src/app/providers/store/base-api.ts
+++ src/app/providers/store/base-api.ts
@@ -1,6 +1,6 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 
-import { baseQueryWithReauth } from '@/services/base-query-with-reauth.ts'
+import { baseQueryWithReauth } from '@/app/providers/store/base-query-with-reauth.ts'
 
 export const baseApi = createApi({
   reducerPath: 'baseApi',
