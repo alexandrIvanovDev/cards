f9196eb7888348b48e1215ea1c7a9d9e763632e8
AlexandrIvanov <alexandr.1996@list.ru>
2024-01-14 11:08:41 +0300

    Add auth service, signIn and signUp

--- src/pages/sign-in/sign-in-page.tsx
+++ src/pages/sign-in/sign-in-page.tsx
@@ -1,28 +1,45 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import s from './sign-in-page.module.scss'
 
-import { routePaths } from '@/app/providers/router/routePaths.tsx'
+import { routePaths } from '@/app/providers/router'
 import { SignInForm } from '@/components/auth/sign-in'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
+import { ProgressBar } from '@/components/ui/progress-bar'
 import { Typography } from '@/components/ui/typography'
+import { useSignInMutation } from '@/services/auth/auth.service.ts'
+import { LoginArgs } from '@/services/auth/auth.types.ts'
 
 export const SignInPage = () => {
-  const onSubmit = () => {}
+  const [signIn, { isLoading }] = useSignInMutation()
+  const navigate = useNavigate()
+
+  const handleLogin = async (data: LoginArgs) => {
+    try {
+      await signIn(data)
+
+      navigate(routePaths.packs)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   return (
-    <Card className={s.wrapper}>
-      <Typography as="h2" variant="large">
-        Sign In
-      </Typography>
-      <SignInForm onSubmit={onSubmit} />
-      <Typography variant="body1" className={s.notification}>
-        {"Don't have an account?"}
-      </Typography>
-      <Button variant="link" className={s.signUp} as={Link} to={routePaths.signUp}>
-        Sign Up
-      </Button>
-    </Card>
+    <>
+      {isLoading && <ProgressBar />}
+      <Card className={s.wrapper}>
+        <Typography as="h2" variant="large">
+          Sign In
+        </Typography>
+        <SignInForm onSubmit={handleLogin} />
+        <Typography variant="body1" className={s.notification}>
+          {"Don't have an account?"}
+        </Typography>
+        <Button variant="link" className={s.signUp} as={Link} to={routePaths.signUp}>
+          Sign Up
+        </Button>
+      </Card>
+    </>
   )
 }
--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -5,10 +5,7 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.flashcards.andrii.es',
     credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-    },
   }),
-  tagTypes: ['Decks'],
+  tagTypes: ['Decks', 'Me'],
   endpoints: () => ({}),
 })
