576c3c9e949c876909c635098fba8f73317691ca
AlexandrIvanov <alexandr.1996@list.ru>
2024-01-16 12:20:38 +0300

    Add card-form

---
+++ src/pages/deck/card-form.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react'
+
+import s from './card-form.module.scss'
+
+import { ControlledTextField } from '@/components/controlled'
+import { Button } from '@/components/ui/button'
+import { CreateCardFormType, useCreateCard } from '@/pages/deck/use-create-card.tsx'
+
+type Props = {
+  onSubmit: (data: CreateCardFormType) => void
+  closeModal: () => void
+  question?: string
+  answer?: string
+  buttonText?: string
+}
+
+export const CardForm: FC<Props> = props => {
+  const { onSubmit, closeModal, answer, question, buttonText } = props
+
+  const { control, handleSubmit, errors } = useCreateCard(question, answer)
+
+  return (
+    <form className={s.modalContent} onSubmit={handleSubmit(onSubmit)}>
+      <ControlledTextField
+        label="Question"
+        control={control}
+        name={'question'}
+        error={errors?.question?.message}
+      />
+      <ControlledTextField
+        label="Answer"
+        control={control}
+        name={'answer'}
+        error={errors?.answer?.message}
+      />
+      <div className={s.modalButtons}>
+        <Button type="button" variant="secondary" onClick={closeModal}>
+          Cancel
+        </Button>
+        <Button type="submit">{buttonText}</Button>
+      </div>
+    </form>
+  )
+}
