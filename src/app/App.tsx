ccb554aaf43bd44b800cc82520ac24a99f2a8275
AlexandrIvanov <alexandr.1996@list.ru>
2024-02-08 14:10:48 +0300

    Add redux persist

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,22 @@
 import { Suspense } from 'react'
 
 import { Provider } from 'react-redux'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { Router } from '@/app/providers/router'
 import { store } from '@/app/providers/store/store.ts'
+import { ProgressBar } from '@/components/ui/progress-bar'
+
+const persistor = persistStore(store)
 
 function App() {
   return (
     <Provider store={store}>
       <Suspense fallback={'Loading...'}>
-        <Router />
+        <PersistGate loading={<ProgressBar />} persistor={persistor}>
+          <Router />
+        </PersistGate>
       </Suspense>
     </Provider>
   )
--- src/app/providers/store/store.ts
+++ src/app/providers/store/store.ts
@@ -1,4 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import { baseApi } from '@/app/providers/store/base-api.ts'
 import { authSlice } from '@/feature/auth/model/slice'
@@ -6,14 +8,24 @@ import { deckSlice } from '@/feature/deck/model/slice'
 import { decksSlice } from '@/feature/decks-list/model/slice'
 import { loadingSlice } from '@/feature/loading/model/slice'
 
+const rootReducer = combineReducers({
+  [baseApi.reducerPath]: baseApi.reducer,
+  decksList: decksSlice.reducer,
+  deck: deckSlice.reducer,
+  loading: loadingSlice.reducer,
+  auth: authSlice.reducer,
+})
+
+const persistConfig = {
+  key: 'cards',
+  storage,
+  blacklist: [baseApi.reducerPath, 'loading', 'auth'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    [baseApi.reducerPath]: baseApi.reducer,
-    decksList: decksSlice.reducer,
-    deck: deckSlice.reducer,
-    loading: loadingSlice.reducer,
-    auth: authSlice.reducer,
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
