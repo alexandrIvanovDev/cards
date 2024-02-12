e16065fe0615eed5d9f3cbc98aa8e63b039be4fd
AlexandrIvanov <alexandr.1996@list.ru>
2024-02-12 16:55:30 +0300

    Fix theme switcher

--- src/components/ui/theme-switcher/theme-switcher.tsx
+++ src/components/ui/theme-switcher/theme-switcher.tsx
@@ -1,19 +1,27 @@
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import * as Switch from '@radix-ui/react-switch'
+import { clsx } from 'clsx'
+import { useTranslation } from 'react-i18next'
 
 import s from './theme-switcher.module.scss'
 
 import { Theme } from '@/app/providers/theme-provider/lib'
 import { useTheme } from '@/common/hooks/use-theme.ts'
-import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
 
 type Props = { className?: string }
 
 export const ThemeSwitcher = ({ className }: Props) => {
   const { theme, toggleTheme } = useTheme()
+  const { t } = useTranslation()
 
   return (
-    <Button onClick={toggleTheme} className={className}>
+    <div className={clsx(s.root, className)}>
       {theme === Theme.DARK ? <SunIcon className={s.icon} /> : <MoonIcon className={s.icon} />}
-    </Button>
+      <Typography variant={'body2'}>{t('Change theme')}</Typography>
+      <Switch.Root className={s.switchRoot} onClick={toggleTheme}>
+        <Switch.Thumb className={s.switchThumb} />
+      </Switch.Root>
+    </div>
   )
 }
